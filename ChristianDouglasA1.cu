117e2fcc31c5fd77cf7630bfabec577528ecf007
Christian Douglas <cedoug13@aol.com>
2020-09-23 11:57:27 -0500

    added more

--- ChristianDouglasA1.cu
+++ ChristianDouglasA1.cu
@@ -5,35 +5,43 @@ const int arrSize = 4096;
 const int blockSize = 1024;
 
 __global__
-void arrayProduct()
+void arrayProduct(int *a, int *b, int *c)
 {
     
-
+    c[threadIdx.x] = a[threadIdx.x] * b[threadIdx.x]; 
     
 
 }
 
 int main()
 {
-    const THREADSIZE = 1023;
     int A[arrSize], B[arrSize], C[arrSize];
-    
+    dim3 dimGrid(3, 1);
+    dim3 dimBlock(blockSize, 1);
+
     for(int i = 0; i < arrSize; i++)
     {
         A[i] = i;
         B[i] = (arrSize-1)+i;
     }
 
-    for(int i = 0; i< 4; i++)
+    for(int i = 0; i< arrSize; i*blockSize)
     {
         cudaMalloc((void**)&a_d, blockSize);
         cudaMalloc((void**)&b_d, blockSize);
         cudaMalloc((void**)&c_d, blockSize);
 
-        cudaMemcpy(a_d, A, THREADSIZE, cudaMemcpyHostToDevice);
-        cudaMemcpy(b_d, B, THREADSIZE, cudaMemcpyHostToDevice);
-        cudaMemcpy(c_d, C, THREADSIZE, cudaMemcpyHostToDevice);
+        cudaMemcpy(a_d, A, blockSize, cudaMemcpyHostToDevice);
+        cudaMemcpy(b_d, B, blockSize, cudaMemcpyHostToDevice);
+        cudaMemcpy(c_d, C, blockSize, cudaMemcpyHostToDevice);
+        arrayProduct<<<dimGrid, dimBlock>>>(a_d, b_d, c_d);
+
+        cudaMemcpy(C, c_d, blockSize, cudaMemcpyDeviceToHost);
+        cudaFree(a_d);
+        cudaFree(b_d);
+        cudaFree(c_d);
     }
+
     // printf("%d", A[0]);
     // printf("%c", ' ');
     // printf("%d", A[arrSize-1]);
